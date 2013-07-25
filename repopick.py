9b665f0e3cb31ec7220ef15f671f0bf6e8f38ab1
Hendrik Hagendorn <finnq@finnq.de>
2013-07-25 22:54:46 +0200

    python3 support
    https://github.com/CyanogenMod/hudson/commit/3fd6e4224678042ec58254f11df9a729b9affb7b

--- repopick.py
+++ repopick.py
@@ -1,17 +1,27 @@
+#!/usr/bin/env python
+
 import sys
-import urllib
-import urllib2
 import json
 import os
 import subprocess
 import re
 
+try:
+    # For python3
+    import urllib.request
+except ImportError:
+    # For python2
+    import imp
+    import urllib2
+    urllib = imp.new_module('urllib')
+    urllib.request = urllib2
+
 for change in sys.argv[1:]:
-    print change
-    f = urllib2.urlopen('http://review.cyanogenmod.com/query?q=change:%s' % change)
-    d = f.read()
+    print(change)
+    f = urllib.request.urlopen('http://review.cyanogenmod.com/query?q=change:%s' % change)
+    d = f.read().decode()
     # gerrit doesnt actually return json. returns two json blobs, separate lines. bizarre.
-    print d
+    print(d)
     d = d.split('\n')[0]
     data = json.loads(d)
     project = data['project']
@@ -20,14 +30,14 @@ for change in sys.argv[1:]:
     while(True):
         retcode = plist.poll()
         pline = plist.stdout.readline().rstrip()
-        ppaths = re.split('\s*:\s*',pline)
+        ppaths = re.split('\s*:\s*', pline.decode())
         if ppaths[1] == project:
             roject = ppaths[0]
             break
         if(retcode is not None):
             break
 
-    print project
+    print(project)
     number = data['number']
     patch_count = 0
     junk = number[len(number) - 2:]
